b426532c5426f9f736199b136900c0789d05740f
VXKAPA <vcodescript@gmail.com>
2022-03-14 09:47:30 +0000

    Regex test

--- src/commands/Social/username.js
+++ src/commands/Social/username.js
@@ -14,8 +14,7 @@ module.exports = class Username extends Command {
 
   async execute({ message, args }) {
     
-    let regex = message.content.match(new RegExp(`^[\wà-úç.(${args[0]});?!+_=@,]$`, "gi"));
-    if(regex) return message.reply(`${e.InsertError} › Seu **nome de usuário** só pode conter **letras**, **números** e **acentos**.`)
+    if(args[0].content.match("(/^[\wà-úç.();?!+_=@,]$/gi")) return message.reply(`${e.InsertError} › Seu **nome de usuário** só pode conter **letras**, **números** e **acentos**.`)
     const name = args[0]
     if(!name) return message.reply(`${e.InsertError} › Você **precisa** inserir o **nome** que deseja **definir**.`)
     if(name.length > 20) return message.reply(`${e.Size} › Seu **nome de usuário** não pode ter **mais** que **20 caracteres**.`)
