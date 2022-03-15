9f7743f6889f19d1be384a418a939908b9e66923
VXKAPA <vcodescript@gmail.com>
2022-03-15 12:09:32 +0000

    Menu fix

--- src/commands/Social/profile.js
+++ src/commands/Social/profile.js
@@ -196,8 +196,9 @@ module.exports = class Profile extends Command {
             if (!users.has(message.author.id)) {
               users.add(message.author.id);
               if (userDBData.social.followers.length == 0) return r.followUp({ content: `${e.Size} › ${USER.id == message.author.id ? "**Você** não possui **seguidores**." : "Este **usuário** não possui **seguidores**."}`, ephemeral: true })
-              let follow = `${USER.id == message.author.id ? "Você" : "Ele(a)"} não o segue.`
-              if (userDBData.social.following.find((x) => x == message.author.id)) follow = `${USER.id == message.author.id ? "Você" : "Ele(a)"} o segue.`
+              /*let follow = `${USER.id == message.author.id ? "Você" : "Ele(a)"} não o segue.`
+              if (userDBData.social.following.find((x) => x == message.author.id)) follow = `${USER.id == message.author.id ? "Você" : "Ele(a)"} o segue.`*/
+              const follow = userDBData.social.following
               const row = new MessageActionRow()
 
               const close = new MessageButton()
@@ -213,7 +214,7 @@ module.exports = class Profile extends Command {
                   .map(
                     (x) =>
                       `> ${e.User} | User: **${this.client.users.cache.get(x).tag
-                      }**\n> ${e.Like} | **${follow}**`
+                      }**\n> ${e.Like} | **${(userDBData.social.following.find((z) => z == this.client.users.cache.get(x).id) ? `${USER.id == message.author.id ? "Você" : "Ele(a)"} o segue.` : `${USER.id == message.author.id ? "Você" : "Ele(a)"} não o segue.`)}**`
                   )
                   .join("\n")}`)
 
