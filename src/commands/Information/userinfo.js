190fafa7e6198c53ecb108bfbe243bdb443dad2c
VXKAPA <vcodescript@gmail.com>
2022-03-17 19:55:32 +0000

    UI 03

--- src/commands/Information/userinfo.js
+++ src/commands/Information/userinfo.js
@@ -7,43 +7,54 @@ registerFont("src/assets/fonts/Montserrat-Medium.ttf", { family: "Medium" });
 registerFont("src/assets/fonts/Montserrat-Regular.ttf", { family: "Regular" });
 
 module.exports = class Userinfo extends Command {
-  constructor(client) {
-    super(client);
-    this.client = client;
+    constructor(client) {
+        super(client);
+        this.client = client;
 
-    this.name = "userinfo";
-    this.category = "Information";
-    this.description = "Veja as informações do usuário inserido.";
-    this.aliases = ["ui"];
-  }
+        this.name = "userinfo";
+        this.category = "Information";
+        this.description = "Veja as informações do usuário inserido.";
+        this.aliases = ["ui"];
+    }
 
-  async execute({ message, args }) {
+    async execute({ message, args }) {
 
-    message.channel.sendTyping()
+        message.channel.sendTyping()
 
-    let USER = await this.client.getUser(args[0], message);
-    if (!USER) USER = message.author;
+        let USER = await this.client.getUser(args[0], message);
+        if (!USER) USER = message.author;
 
-    const canvas = createCanvas(1000, 600);
-    const ctx = canvas.getContext("2d");
+        const canvas = createCanvas(1000, 600);
+        const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = '#1f2430';
-    ctx.fillRect(0, 0, 1000, 600)
+        ctx.fillStyle = '#1f2430';
+        ctx.fillRect(0, 0, 1000, 600)
 
-    ctx.arc(200, 190, 130, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.save();
-    ctx.clip();
+        ctx.arc(200, 190, 130, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.save();
+        ctx.clip();
 
-    const avatar = await loadImage(
-        USER.displayAvatarURL({ format: "jpeg", size: 2048 })
-      );
-      ctx.drawImage(avatar, 70, 60, 260, 260);
+        const avatar = await loadImage(
+            USER.displayAvatarURL({ format: "jpeg", size: 2048 })
+        );
+        ctx.drawImage(avatar, 70, 60, 260, 260);
 
-    ctx.restore()
-    
-    const attach = new MessageAttachment(canvas.toBuffer(), "UserInfo.png");
+        ctx.textAlign = "left";
+        ctx.font = '40px "Bold"';
+        ctx.fillStyle = "#FFFFFF";
+        await this.client.renderEmoji(ctx, USER.username, 360, 180);
+        const w = ctx.measureText(USER.username)
 
-    await message.reply({ files: [attach] });
-  }
+        ctx.textAlign = "left";
+        ctx.font = '20px "Regular"';
+        ctx.fillStyle = "#CCCCCC";
+        ctx.fillText(USER.discriminator, 360 + w, 180);
+
+        ctx.restore()
+
+        const attach = new MessageAttachment(canvas.toBuffer(), "UserInfo.png");
+
+        await message.reply({ files: [attach] });
+    }
 };
