d617626e8be50c57f63cc3fe798b8de8cc966b8f
VXKAPA <vcodescript@gmail.com>
2022-03-17 20:24:39 +0000

    UI 12

--- src/commands/Information/userinfo.js
+++ src/commands/Information/userinfo.js
@@ -38,7 +38,7 @@ module.exports = class Userinfo extends Command {
         const avatar = await loadImage(
             USER.displayAvatarURL({ format: "jpeg", size: 2048 })
         );
-        ctx.drawImage(avatar, 70, 60, 250, 250);
+        ctx.drawImage(avatar, 75, 65, 250, 250);
         ctx.restore()
 
         ctx.font = '50px "Bold"';
