c8d8e3a3463c45aa8241a36605fc8a1e1e2dd344
VXKAPA <vcodescript@gmail.com>
2022-03-18 22:23:49 +0000

    Bug command fix

--- src/commands/Bot/bug.js
+++ src/commands/Bot/bug.js
@@ -3,7 +3,7 @@ const e = require('../../utils/Emojis');
 const Embed = require('../../structures/Embed')
 
 module.exports = class Bug extends Command {
-	constructor (client) {
+	constructor(client) {
 		super(client);
 		this.client = client;
 
@@ -13,21 +13,23 @@ module.exports = class Bug extends Command {
 		this.aliases = ['reportbug', '🐛'];
 	}
 
-	async execute ({ message, args }) {
+	async execute({ message, args }) {
 
-        if(!args[0]) return message.reply(`${e.InsertError} › Insira o **BUG** que deseja **reportar** ao meu **desenvolvedor**.`)
+		if (!args[0]) return message.reply(`${e.InsertError} › Insira o **BUG** que deseja **reportar** ao meu **desenvolvedor**.`)
 		const bug = args.slice(0).join(" ");
-		const image = message.attachments.first()
-		if(bug.length < 10) return message.reply(`${e.Size} › O **BUG** deve ter no mínimo **10 caracteres**.`)
+		if (bug.length < 10) return message.reply(`${e.Size} › O **BUG** deve ter no mínimo **10 caracteres**.`)
+
+		const attachment = message.attachments.first();
+		const url = attachment ? attachment.url : null;
 
 		const channel = this.client.channels.cache.get(process.env.BUG_ID);
 
 		const embed = new Embed(message.author)
-		.setAuthor({name: this.client.user.username, iconURL: this.client.user.avatarURL()})
-		.setDescription(`${e.Bug} › Um novo **BUG** foi encontrado.\n\n> ${e.BugInfo} › BUG encontrado: **${bug}**\n> ${e.User} › Autor: **${message.author.tag}**\n> ${e.Earth} › Guilda: **${message.guild.name}**`)
-		.setImage(image.url)
-		await channel.send({content: `<@${process.env.OWNER_ID}>`, embeds: [embed]})
+			.setAuthor({ name: this.client.user.username, iconURL: this.client.user.avatarURL() })
+			.setDescription(`${e.Bug} › Um novo **BUG** foi encontrado.\n\n> ${e.BugInfo} › BUG encontrado: **${bug}**\n> ${e.User} › Autor: **${message.author.tag}**\n> ${e.Earth} › Guilda: **${message.guild.name}**`)
+			.setImage(url)
+		await channel.send({ content: `<@${process.env.OWNER_ID}>`, embeds: [embed] })
 		return message.reply(`${e.Success} › BUG **enviado** ao meu desenvolvedor com **sucesso**, obrigada pela **ajuda**! :>`)
-		
+
 	}
 };
